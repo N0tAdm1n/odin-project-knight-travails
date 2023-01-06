58b51fc441ffb470b9423a7db51c41906bd5dd2d
N0tAdm1n <n0tadm1n29@gmail.com>
2023-01-06 19:54:55 +0530

    Add KnightMoves()

---
+++ knight-travails.js
@@ -0,0 +1,14 @@
+function KnightMove() {
+  return {
+    squareName: null,
+    nextMove1: null,
+    nextMove2: null,
+    nextMove3: null,
+    nextMove4: null,
+    nextMove5: null,
+    nextMove6: null,
+    nextMove7: null,
+    nextMove8: null,
+    previousMove: null,
+  };
+}
