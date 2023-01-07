aa2a4f289d3f13beb2f0fe484ac0fbf4bb6f84b4
N0tAdm1n <n0tadm1n29@gmail.com>
2023-01-07 17:41:14 +0530

    Add comments and export knightTravails() factory

--- knight-travails.js
+++ knight-travails.js
@@ -1,3 +1,4 @@
+// factory function to make nodes for moves
 function MoveNode(currentMove, previousMove = null) {
   return {
     squareName: currentMove,
@@ -13,12 +14,15 @@ function MoveNode(currentMove, previousMove = null) {
   };
 }
 
+// factory which does every calculation and return only a single function knightMoves()
 function knightTravails() {
+  // stores the starting move and whole tree is build on it
   let start;
 
+  // takes two arguements - starting square and ending sqaure
   function knightMoves(startMove, endMove) {
     start = MoveNode(startMove);
-    // console.log(start);
+
     let endNode = makeMoveTree(start, endMove);
 
     let path = [];
@@ -33,23 +37,27 @@ function knightTravails() {
     path.forEach((node) => console.log(node));
   }
 
+  // makes a tree of all posible moves until the endMove is encountered
+  // tree is made using breadth first inserting
   function makeMoveTree(move, end) {
-    let queue = [];
+    let queue = []; // queue for breadth first approach
 
+    // push the starting move in the queue
     queue.push(move);
-    // console.log(queue.shift());
-    // console.log(move.squareName);
-    // console.log(end);
-    // console.log(checkEqual(move.squareName, end));
 
+    // loop will run until queue is not empty,
+    // but will the function will always return if the endNode is found
+    // which will always happen
     while (queue.length > 0) {
+      // remove the first element from the queue
       let tempMove = queue.shift();
 
+      // check if the sqaure is same as end, if is then return that sqaure
       if (checkEqual(tempMove.squareName, end)) return tempMove;
 
       fillNextMove(tempMove);
-      //   console.log(tempMove.currentMove);
 
+      // if the next moves exists push them into the queue
       if (tempMove.nextMove1) queue.push(tempMove.nextMove1);
       if (tempMove.nextMove2) queue.push(tempMove.nextMove2);
       if (tempMove.nextMove3) queue.push(tempMove.nextMove3);
@@ -59,10 +67,9 @@ function knightTravails() {
       if (tempMove.nextMove7) queue.push(tempMove.nextMove7);
       if (tempMove.nextMove8) queue.push(tempMove.nextMove8);
     }
-
-    return move;
   }
 
+  // add the next possible eight moves if they exist to the given move
   function fillNextMove(move) {
     if (checkMove([move.squareName[0] + 2, move.squareName[1] + 1]))
       move.nextMove1 = MoveNode(
@@ -113,11 +120,13 @@ function knightTravails() {
       );
   }
 
+  // function to check if the move is possible (should be within board)
   function checkMove(arr) {
     if (arr[0] >= 0 && arr[0] < 8 && arr[1] >= 0 && arr[1] < 8) return true;
     else return false;
   }
 
+  // function to match the array of squares
   function checkEqual(arrA, arrB) {
     return JSON.stringify(arrA) === JSON.stringify(arrB);
   }
@@ -127,5 +136,7 @@ function knightTravails() {
   };
 }
 
+export { knightTravails };
+
 let test = knightTravails();
-test.knightMoves([3, 3], [4, 3]);
+test.knightMoves([3, 3], [3, 3]);
